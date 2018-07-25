e44b14f84efd6a1d2de82b79e01c7b8bc0a0539e
Arturo Salinas <jasalina@ncsu.edu>
2018-07-25 12:06:07 -0400

    hash table building

--- Project4/src/csc316/project4/Project4.java
+++ Project4/src/csc316/project4/Project4.java
@@ -26,6 +26,7 @@ public class Project4 {
     private static ArrayList<LinkedList<String>> hashTable;
     
     private static int dictionaryWords;
+    
     /**Found by multiplying number of dictionary characters by 0.2, adding that to number of dictionary characters and then finding the 
      * the closest prime number. 
      */
@@ -51,27 +52,57 @@ public class Project4 {
         outfile = outfile.trim();
         
         buildHashTable();
+        System.out.println("HashCode Check: " + useHashFunction("ABC"));
+        
         
         console.close();
     }
     
-    
+    /**
+     * Method that builds the hashTable for the dictionary. 
+     */
     public static void buildHashTable() {
         try {
             Scanner dictionaryScan = new Scanner(new FileInputStream(dictionaryFile));
             dictionaryWords = 0;
+            //Initializing the hashTable
             hashTable = new ArrayList<LinkedList<String>>(sizeCapM);
-            
-            while(dictionaryScan.hasNext()) {
+            for (int i = 0; i < sizeCapM; i++){
+                LinkedList<String> listInit = new LinkedList<String>();
                 
-                dictionaryWords++;
+                hashTable.add(listInit);
             }
             
+            int totalCharsLength = 0;
+            while(dictionaryScan.hasNext()) {
+                String currentWord = dictionaryScan.next();
+                int hash = useHashFunction(currentWord);
+                totalCharsLength =+ currentWord.length();
+                
+                //Use get to retrieve the linkedList at a position and store to temp
+                //Append new word to temp list and then use set to reinsert the temp list to the spot
+                dictionaryWords++;
+            }
+            System.out.println("Number of Words in Dictionary: " + dictionaryWords);
         } catch (FileNotFoundException e) {
             System.out.println("The dictionary file was not found.");
         }
         
     }
+    
+    
+    public static int useHashFunction(String word) {
+        //Since each ascii character is typically 7 bits long
+        int radix = 128;
+        int wordVal = 0;
+        for(int i = 0, j = (word.length() - 1); i < word.length(); i++) {
+            int charVal = (int) word.charAt(i);
+            wordVal =(int) (+ charVal * Math.pow(radix, j));
+        }
+        
+        return (wordVal % sizeCapM);
+    }
+    
     
 }
 
