76f56a31f26e3ab199cf1afa46c3fbb0c09574da
Arturo Salinas <jasalina@ncsu.edu>
2018-07-25 16:33:26 -0400

    working on rules

--- Project4/src/csc316/project4/Project4.java
+++ Project4/src/csc316/project4/Project4.java
@@ -3,8 +3,10 @@
  */
 package csc316.project4;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -24,13 +26,16 @@ public class Project4 {
     private static String outfile = "";
     /**Hash table that stores words of dictionary*/
     private static ArrayList<LinkedList<String>> hashTable;
-    
+    /**Keeps count of the number of words in the dictionary*/
     private static int dictionaryWords;
-    
-    /**Found by multiplying number of dictionary characters by 0.2, adding that to number of dictionary characters and then finding the 
+    /** Found by multiplying number of dictionary characters by 0.2, adding that to number of dictionary characters and then finding the 
      * the closest prime number. 
      */
     private static final int sizeCapM = 30181; 
+    
+    private static int wordsInFile;
+    
+    private static int numLookUps;
     
     
     /**
@@ -54,9 +59,13 @@ public class Project4 {
         buildHashTable();
         System.out.println("Yuki is " + hashTable.get(useHashFunction("Yuki")).toString());
         System.out.println("yuh is " + hashTable.get(useHashFunction("yuh")).toString());
+        System.out.println("Index of Braggart " + hashTable.get(useHashFunction("braggart")).indexOf("braggart"));
         System.out.println("roadblock is " + hashTable.get(useHashFunction("roadblock")).toString());
         System.out.println("don't is " + hashTable.get(useHashFunction("don't")).toString());
         System.out.println("Contents of space 25 is" + hashTable.get(25).toString());
+        
+        
+//        System.out.println("Cleaned: " + removePunctuation("Boy! howdy?_taco don't mang, it ain't"));
         
         console.close();
     }
@@ -94,7 +103,13 @@ public class Project4 {
         
     }
     
-    
+    /**
+     * Method that creates a compressed hash-code to use in deciding where to store a word.
+     * Applies the Polynomial Hash Code discussed in class and the divide to compress method. 
+     * 
+     * @param word is the word to make the hashCode of
+     * @return the integer hash code of the word passed in. 
+     */
     public static int useHashFunction(String word) {
         //Since each ascii character is typically 7 bits long
         int radix = 128;
@@ -111,6 +126,90 @@ public class Project4 {
         
         //Absolute value to avoid negative compressed hashvalues
         return Math.abs((wordVal % sizeCapM));
+    }
+    
+    
+    public static void spellChecker() {
+        try {
+            Scanner checkFile = new Scanner(new FileInputStream(filename));
+            PrintStream output = new PrintStream(new File(outfile));
+            wordsInFile = 0;
+            numLookUps = 0;
+            int numProbes = 0;
+            while(checkFile.hasNextLine()) {
+                String checkLine = checkFile.nextLine();
+                checkLine = removePunctuation(checkLine);
+                Scanner lineScanner = new Scanner(checkLine);
+                while(lineScanner.hasNext()) {
+                    String word = lineScanner.next();
+                    int currentProbes = lookUpWord(word);
+                    
+                    //First Rule
+                    if(currentProbes <= 0 && Character.isUpperCase(word.charAt(0))) {
+                        word = Character.toLowerCase(word.charAt(0)) + word.substring(1);
+                        currentProbes = lookUpWord(word);
+                    }
+                    //Second Rule
+                    if(currentProbes <= 0 && word.substring(word.length() - 2).equals("'s")) {
+                        word = word.substring(0, word.length() - 2);
+                    }
+                    
+
+                    
+                    
+                    
+                    numLookUps++;
+                }
+                //it should count the number of probes in the hash
+                //table. For the purposes of counting, a probe occurs whenever a text word is compared to a word
+                //in the table. A probe does not occur when a text word is compared to an empty table entry.
+                
+                //This should hold the conditions of rechecking as well
+                //lookups++ in every if else of rechecking word
+                
+                
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error reading the input file to spellcheck.");
+        }
+        
+    }
+    
+    /**
+     * Method that looks in the hashTable to see if the word is present.
+     * 
+     * @param word the word to lookup
+     * @return the number of probes that it took to look up a word. 0 if the word was compared to an empty row.
+     */
+    public static int lookUpWord(String word) {
+        //If word is not in dictionary return 0
+        LinkedList<String> listToProbe = hashTable.get(useHashFunction(word));
+        int probes = 0;
+        if(listToProbe.isEmpty()) {
+            return 0;
+        } else if(listToProbe.indexOf(word) == -1){
+            
+                probes = (listToProbe.size() + 1);
+                //Looked through entire list and compared against all elements but didn't find
+                return (probes * -1);
+            
+        } else {
+            //Since at least one comparison has to be made.
+            probes = (listToProbe.indexOf(word) + 1);
+            return probes;
+        
+        }
+        
+    }
+    
+    /**
+     * Simple method that removes all punctuation from a string line.
+     * @param line the string to remove the punctuation from.
+     * @return the initial string without any punctuation except "'"
+     */
+    public static String removePunctuation(String line) {
+        line = line.replaceAll("[\\p{Punct}&&[^']]+", " ");
+        return line;
     }
     
     
